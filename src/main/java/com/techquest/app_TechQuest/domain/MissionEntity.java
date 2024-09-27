f194bac486cf585ced4a7135843487692a98c2f9
alejandroava <alejandroalzate.ava@gmail.com>
2024-09-26 20:42:40 -0500

    Add MissionEntity with anotation

--- src/main/java/com/techquest/app_TechQuest/domain/MissionEntity.java
+++ src/main/java/com/techquest/app_TechQuest/domain/MissionEntity.java
@@ -1,30 +1,27 @@
 package com.techquest.app_TechQuest.domain;
 
-import com.techquest.app_TechQuest.utils.Role;
+import com.techquest.app_TechQuest.utils.MissionDifficulty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity(name = "users")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserEntity {
+@Entity(name = "missions")
+public class MissionEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
-    private String name;
-    @Column(nullable = false)
-    private String email;
-    @Column(nullable = false)
-    private String password;
+    private String title;
+
+    private String description;
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
-    private Role role;
-
+    private MissionDifficulty difficulty;
 }
