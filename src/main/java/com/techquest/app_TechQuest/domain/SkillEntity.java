3e20d80783106051577749493731997496c15fd1
alejandroava <alejandroalzate.ava@gmail.com>
2024-09-26 20:46:11 -0500

    Add SkillEntity with anotation

---
+++ src/main/java/com/techquest/app_TechQuest/domain/SkillEntity.java
@@ -0,0 +1,24 @@
+package com.techquest.app_TechQuest.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "skills")
+public class SkillEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String description;
+}
