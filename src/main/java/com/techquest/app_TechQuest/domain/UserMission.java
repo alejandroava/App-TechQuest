66b8a74f55930e5bc5b1c31634cef70a6d330baf
alejandroava <alejandroalzate.ava@gmail.com>
2024-09-26 20:50:20 -0500

    Add table intermedia UserMission

---
+++ src/main/java/com/techquest/app_TechQuest/domain/UserMission.java
@@ -0,0 +1,29 @@
+package com.techquest.app_TechQuest.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class UserMission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private MissionEntity mission;
+
+    private UserEntity user;
+
+
+}
