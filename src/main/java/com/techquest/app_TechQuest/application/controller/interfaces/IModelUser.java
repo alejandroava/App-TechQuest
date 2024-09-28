33da055f038c7e2dbd5fbd4e4a1186a1e9696276
alejandroava <alejandroalzate.ava@gmail.com>
2024-09-28 01:00:47 -0500

    Add generic create in controller , ImodelUser with interface create and controller

---
+++ src/main/java/com/techquest/app_TechQuest/application/controller/interfaces/IModelUser.java
@@ -0,0 +1,7 @@
+package com.techquest.app_TechQuest.application.controller.interfaces;
+
+import com.techquest.app_TechQuest.application.controller.generic.Create;
+import com.techquest.app_TechQuest.application.dto.request.AdminCreateSkillDTO;
+
+public interface IModelUser extends Create<AdminCreateSkillDTO> {
+}
