c7d6f7387431a4dced128e03bcdba46552c6fbd6
alejandroava <alejandroalzate.ava@gmail.com>
2024-10-02 21:23:06 -0500

    Add configuration login

---
+++ src/main/java/com/techquest/app_TechQuest/infrastructure/configuration/applicationConfig/ApplicationConfig.java
@@ -0,0 +1,58 @@
+package com.techquest.app_TechQuest.infrastructure.configuration.applicationConfig;
+
+import com.techquest.app_TechQuest.domain.model.UserEntity;
+import com.techquest.app_TechQuest.infrastructure.persistence.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+//configuracion basica para login
+
+@Configuration
+public class ApplicationConfig {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    //Permite el manejo del usuario que va estar autenticado, dice que puede y que no puede hacer el usuario
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authentication) throws Exception{
+        return authentication.getAuthenticationManager();
+    }
+
+    public AuthenticationProvider authenticationProvider (){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
+
+        return daoAuthenticationProvider;
+    }
+
+    //Metodo para cargar los detalles del usuario mediante la autenticacion
+    public UserDetailsService userDetailsService() {
+        return email -> {
+            UserEntity user = userRepository.findByEmail(email);
+            if (user == null) {
+                throw new UsernameNotFoundException("User not found");
+            }
+            return user;
+        };
+    }
+
+}
